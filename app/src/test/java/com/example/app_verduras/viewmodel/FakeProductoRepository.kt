53744ba13a5ab2c154ec5781e53ceee8e6d1a1f9
Martin Coronel <coronelmartin@hotmail.cl>
2025-11-18 18:00:51 -0300

    repo producto falso

--- app/src/test/java/com/example/app_verduras/viewmodel/FakeProductoRepository.kt
+++ app/src/test/java/com/example/app_verduras/viewmodel/FakeProductoRepository.kt
@@ -1,4 +1,10 @@
 package com.example.app_verduras.viewmodel
 
+import com.example.app_verduras.Model.Producto
+import com.example.app_verduras.repository.ProductoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
 class FakeProductoRepository {
 }
